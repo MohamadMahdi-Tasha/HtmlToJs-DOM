e981d46cb7ea4db7c9260b3e8d1475709a62761e
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-11 17:08:11 +0430

    idk

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -1,1 +1,24 @@
 // Codes By MAHDI Tasha
+// Variables
+const htmlInput = document.getElementById('html-input');
+const jsInput = document.getElementById('JS-input');
+const submitButton = document.querySelector('button[type="submit"]');
+const tagsInText = [];
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const string = htmlInput.value;
+    const formattedStr = string.match(/<[a-zA-Z]>(.*?)<\/[a-zA-Z]>/g)
+
+    formattedStr.forEach(item => {
+        const tagNameEnd = item.indexOf('>')
+        tagsInText.push(item.slice(1, tagNameEnd));
+    })
+
+
+    tagsInText.forEach(item => {
+        jsInput.textContent = `document.createElement(${item})`
+        console.log(item)
+    })
+})
